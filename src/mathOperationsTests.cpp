e91651d4905130aa39ff58237a4817e91defe84d
Filip Novak <fifix.tp@gmail.com>
2024-03-18 17:24:28 +0100

    Created tests file and added comments

---
+++ src/mathOperationsTests.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file mathLibTests.cpp
+ * 
+ * @brief Performs tests on math operations from math_operations.h.
+ *  This file is used for testing all operations library. It tests basic cases as well as edgecases, which might 
+ *  possibly give incorrect result. It also tests incorrect inputs and if they are handled properly.
+ * 
+ * @author xfignam00
+ * @author xmalegt00
+ * @author xnovakf00
+ * @author xskovaj00
+ */
+
+#include "math_operations.h"
