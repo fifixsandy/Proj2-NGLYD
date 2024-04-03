8bd618bca3aa4e3d5e5b9a1a01f5938ae062e073
Tibor <tibormalega3@gmail.com>
2024-04-03 19:30:37 +0200

    Added error handling to header file of math_operations

--- src/math_operations.cpp
+++ src/math_operations.cpp
@@ -1,1 +1,1 @@
-//soon
+//soonG
--- src/math_operations.h
+++ src/math_operations.h
@@ -16,6 +16,21 @@
  * @author xskovaj00
  */
 
+
+/**
+ * @brief Enumerates error codes for mathematical operations in the MathOps class.
+ *
+ * This enumeration provides a way of reporting errors encountered while
+ * executing mathematical operations. Each enumerator corresponds
+ * to a specific error condition that can be encountered.
+ */
+enum MathErrorCode {
+    SUCCESS = 0, ///< No error occured during mathematical operation.
+    DIVISION_BY_ZERO, ///< Division by zero occured while calculating.
+    NEGATIVE_ROOT_INPUT, ///< Negative number was provided as an input for calculating root.
+    NEGATIVE_FACTORIAL_INPUT, ///< Negative number was provided as an input for calculating factorial.
+};
+
 class MathOps {
 public:
     
@@ -44,7 +59,7 @@ public:
     * @param b The second double operand (divisor).
     * @return The quotient of a divided with b as a double.
     */ 
-    double divide(double a, double b);
+    double divide(double a, double b, MathErrorCode &errCode);
 
     /**
     * @brief Performs multiplication of two doubles.   
@@ -61,7 +76,7 @@ public:
     * @param a Double operand.
     * @return The result of square root of a as a double.
     */ 
-    double squareRoot(double a);
+    double squareRoot(double a, MathErrorCode &errCode);
 
     /**
     * @brief Performs factorial of an int.   
@@ -69,7 +84,7 @@ public:
     * @param a Int operand.
     * @return The factorial of a as an unsigned long long.
     */ 
-    unsigned long long factorial(int a);
+    unsigned long long factorial(int a, MathErrorCode &errCode);
         
     /**
     * @brief Performs nth power of a double and int.  
@@ -87,7 +102,7 @@ public:
     * @param n The second int operand (root).
     * @return The n-th root of a as a double.
     */ 
-    double nthRoot(double a, int n);
+    double nthRoot(double a, int n, MathErrorCode &errCode);
 
     /**
     * @brief Calculates the remainder by dividing two ints.  
@@ -96,14 +111,10 @@ public:
     * @param b The second int operand (divisor).
     * @return The remainder of a divided by b as an int.
     */ 
-    int modulo(int a, int b);
+    int modulo(int a, int b, MathErrorCode &errCode);
     
 private:
 
 };
-
-#define MATH_ERROR -1.0
-#define DivisionByZero "ERROR: Division by zero"
-#define NegativeInRoot "ERROR: Root of negative number"
 
 #endif //MATH_OPERATIONS_H
