9294898ee2743bab45a3641044b45deac035afc0
Filip Novak <fifix.tp@gmail.com>
2024-04-13 18:31:51 +0200

    Added function for calculating mean from numbers from input

--- src/PROFILING/profiling.cpp
+++ src/PROFILING/profiling.cpp
@@ -14,11 +14,55 @@
  */
 
 #include "../LIBS/math_operations.h"
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 MathOps m;
+
+/**
+* @brief Calculates mean of given numbers
+* 
+* @param vector Vector of floats to calculate mean from
+* @param count	Count of the numbers
+* @return		Mean of the numbers
+*/
+float mean(std::vector<double> vector, double count){
+
+	float sum = 0.0; // sum of all the numbers
+
+	for(float num : vector)
+	{
+		sum = m.add(sum, num);
+	}
+	
+	MathErrorCode ERR = SUCCESS;
+	
+	float mean = m.divide(sum, count, ERR);
+	
+	if(ERR != SUCCESS){
+		return ERR;
+	}
+	else{
+		return mean;
+	}
+}
+
 int main() {
-	std::cout << "Sample profiling source code" <<std::endl;
+	std::vector<double> numbers;
+
+	double num;
+	double count;
+
+	while(std::cin >> num){
+		numbers.push_back(num);
+		count++;
+	}
+
+	float meanN = mean(numbers, count);
+
+	std::cout << meanN <<std::endl;
+
 	return 0;
 }
 
