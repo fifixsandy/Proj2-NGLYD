661d67992416e4c9525c2500b7b1ebafb248a028
Filip Novak <fifix.tp@gmail.com>
2024-04-13 17:40:56 +0200

    Added comments for file

--- src/PROFILING/profiling.cpp
+++ src/PROFILING/profiling.cpp
@@ -1,7 +1,24 @@
+/**
+ * @file profiling.cpp
+ * 
+ * @brief Source code for calculating standard deviation
+ *
+ * This file calculates a standard deviation from numbers given through stdin.
+ * It is made for profiling purposes and uses math_operations.h library.
+ * It takes at most XXX numbers of XXX size.
+ * 
+ * @author xfignam00
+ * @author xmalegt00
+ * @author xnovakf00
+ * @author xskovaj00
+ */
+
+#include "../LIBS/math_operations.h"
 #include<iostream>
 
+MathOps m;
 int main() {
-	std::cout << "Sample profiling source code" << std::endl;
+	std::cout << "Sample profiling source code" <<std::endl;
 	return 0;
 }
 
